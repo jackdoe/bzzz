91e69df597c57dad792a9ab3aec2246cba2fce9e
borislav nikolov <jack@sofialondonmoskva.com>
2014-10-18 14:11:41 +0200

    add pagination test

--- test/bzzz/core_test.clj
+++ test/bzzz/core_test.clj
@@ -22,6 +22,34 @@
       (is (= "john doe" (:name_store_index (first (:hits ret)))))
       (is (= "jack doe foo" (:name_store_index (last (:hits ret)))))))
 
+  (testing "search-or-pages"
+    (let [ret-page-0 (search :index test-index-name
+                      :default-field "name_store_index"
+                      :default-operator "or"
+                      :size 1
+                      :page 0
+                      :query "john doe")
+          ret-page-1 (search :index test-index-name
+                             :default-field "name_store_index"
+                             :default-operator "or"
+                             :size 1
+                             :page 1
+                             :query "john doe")
+          ret-page-2 (search :index test-index-name
+                             :default-field "name_store_index"
+                             :default-operator "or"
+                             :size 1
+                             :page 2
+                             :query "john doe")]
+      (is (= 2 (:total ret-page-0)))
+      (is (= 2 (:total ret-page-1)))
+      (is (= 2 (:total ret-page-2)))
+      (is (= 1 (count (:hits ret-page-0))))
+      (is (= 1 (count (:hits ret-page-1))))
+      (is (= 0 (count (:hits ret-page-2))))
+      (is (= "john doe" (:name_store_index (first (:hits ret-page-0)))))
+      (is (= "jack doe foo" (:name_store_index (last (:hits ret-page-1)))))))
+
   (testing "search-and"
     (let [ret (search :index test-index-name
                       :default-field "name_store_index"

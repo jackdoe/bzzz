1ab84213b9e874f611fdd08936f2775e48a60111
borislav nikolov <jack@sofialondonmoskva.com>
2014-10-25 02:01:49 +0200

    add explain to the random-score-query

--- src/bzzz/random_score_query.clj
+++ src/bzzz/random_score_query.clj
@@ -1,5 +1,5 @@
 (ns bzzz.random-score-query
-  (:import (org.apache.lucene.search Query IndexSearcher Weight Scorer)
+  (:import (org.apache.lucene.search Query IndexSearcher Weight Scorer Explanation ComplexExplanation)
            (org.apache.lucene.index AtomicReaderContext)
            (org.apache.lucene.util Bits)))
   
@@ -9,6 +9,19 @@
     (createWeight [^IndexSearcher searcher]
       (let [sub-weight (.createWeight subq searcher)]
         (proxy [Weight][]
+          (explain [^AtomicReaderContext ctx doc]
+            (let [e (Explanation. 0 "unknown-random-value")
+                  sub-explain (.explain sub-weight ctx doc)]
+              ;; because of the nature of random-score-query,
+              ;; we cant actually reproduce the same score
+              ;; if we had deterministic scorer we can just do:
+              ;;
+              ;; (let [s (.scorer this ctx (.getLiveDocs (.reader ctx)))]
+              ;;   (if (= (.advance s doc) doc)
+              ;;     (.addDetail e (Explanation. (score s), "random value"))
+              ;;     (Explanation. 0 "no matching term")))
+              (.addDetail e sub-explain)
+              e))
           (getValueForNormalization [] 1)
           (getQuery [] subq)
           (normalize [norm boost])

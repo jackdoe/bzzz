78b81a1ed8e50ea6093ac038d2207e1fa812e6c5
borislav nikolov <jack@sofialondonmoskva.com>
2014-11-04 01:11:05 +0100

    use lucene 4.10.2

--- project.clj
+++ project.clj
@@ -10,12 +10,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.lucene/lucene-core "4.10.1"]
-                 [org.apache.lucene/lucene-facet "4.10.1"]
-                 [org.apache.lucene/lucene-highlighter "4.10.1"]
-                 [org.apache.lucene/lucene-queryparser "4.10.1"]
-                 [org.apache.lucene/lucene-analyzers-common "4.10.1"]
-                 [org.apache.lucene/lucene-expressions "4.10.1"]
+                 [org.apache.lucene/lucene-core "4.10.2"]
+                 [org.apache.lucene/lucene-facet "4.10.2"]
+                 [org.apache.lucene/lucene-highlighter "4.10.2"]
+                 [org.apache.lucene/lucene-queryparser "4.10.2"]
+                 [org.apache.lucene/lucene-analyzers-common "4.10.2"]
+                 [org.apache.lucene/lucene-expressions "4.10.2"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.2.4"]

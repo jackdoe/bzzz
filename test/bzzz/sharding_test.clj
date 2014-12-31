33f66fab746334d599351417317d6dd59ce16635
borislav nikolov <jack@sofialondonmoskva.com>
2014-12-31 20:19:34 +0100

    test the internal sharding

---
+++ test/bzzz/sharding_test.clj
@@ -0,0 +1,50 @@
+(ns bzzz.sharding-test
+  (:use clojure.test
+        bzzz.core
+        bzzz.util
+        bzzz.analyzer
+        bzzz.index-store
+        bzzz.index-search
+        bzzz.query))
+
+(def test-index-name "__lein-test-testing-index-sharding-test")
+
+(deftest test-app
+  (testing "cleanup-before"
+    (delete-all test-index-name))
+
+  (testing "sharding"
+    (dotimes [x 100]
+      (let [doc-generator (fn [n]
+                            {:name "jack"
+                             :id (str "id-" (* 100 n))})
+            list-generator (fn [n]
+                             (into []
+                                   (for [i (range n)]
+                                     (doc-generator i))))
+            docs (list-generator 1000)]
+        (let [r (store {:index test-index-name
+                        :must-refresh true
+                        :documents docs
+                        :number-of-shards 3})
+              r1 (store {:index test-index-name
+                         :must-refresh true
+                         :documents docs
+                         :number-of-shards 3})]
+          (is (= 3 (count r)))
+          (let [rc0 (into [] (for [item r]
+                               (for [[k v] item]
+                                 (:attempt-to-write v))))
+                rc1 (into [] (for [item r1]
+                               (for [[k v] item]
+                                 (:attempt-to-write v))))
+                m0 (into {} (for [[item] rc0] [item true]))
+                m1 (into {} (for [[item] rc1] [item true]))]
+            (is (= m0 m1)))
+
+          (doseq [item r]
+            (doseq [[k v] item]
+              (is (> (:attempt-to-write v) 300))))))))
+    
+  (testing "cleanup-after"
+    (delete-all test-index-name)))

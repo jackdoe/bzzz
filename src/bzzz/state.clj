99f9c42835d71bfa190ffdc70fcb43fdbfc92c15
Caio Romão <caioromao@gmail.com>
2015-01-17 13:55:53 +0100

    Add support for temporary state and utility functions
    This patch follows @jackdoe's suggestions in PR#1 and adds support for
    writing to a temporary state and later on replacing/merging with the
    global read-only state.

--- src/bzzz/state.clj
+++ src/bzzz/state.clj
@@ -3,11 +3,10 @@
   (use [clojure.set :only (rename-keys)])
   (:import (bzzz.java.query TermPayloadClojureScoreQuery)))
 
-;; FIXME Depending on the usage pattern, this can be terribly inefficient.
-;;       If this becomes a problem, we should consider a flow where we
-;;       write to the RW state and just merge into RO once the data pumping
-;;       is complete.
 ;; TODO Maybe add support for @hosts?
+;; FIXME Deep-merges don't seem very useful anymore. Drop 'em?
+
+(def temporary-state (atom {}))
 
 (defn ro-merge
   "Merge the given data map into the global read-only state.
@@ -53,3 +52,37 @@
   (let [ro-state (into {} TermPayloadClojureScoreQuery/EXPR_GLOBAL_STATE_RO)]
     (TermPayloadClojureScoreQuery/replace_expr_global_state_ro
       (rename-keys ro-state { (keyword from_key) (keyword to_key) }))))
+
+(defn temp-assoc-in
+  "Associates input data into the temporary map.
+
+  Example:
+  (temp-assoc-in [ [[:foo :bar] 1] [[:foo :baz] 2] ])
+  ;;=> { :foo { :bar 1, :baz 2 } }
+  "
+  [data]
+  (doseq [[k v] data] (swap! temporary-state assoc-in k v)))
+
+(defn temp-empty
+  "Erases the data saved in the temporary state."
+  []
+  (swap! temporary-state empty))
+
+(defn ro-replace-with-temp
+  "Replaces the global read-only state with the contents of the temporary state."
+  []
+  (TermPayloadClojureScoreQuery/replace_expr_global_state_ro @temporary-state))
+
+(defn ro-merge-with-temp
+  "Merges the temporary state with the global read-only one.
+
+  Refer to bzzz.state.ro-merge for details"
+  []
+  (ro-merge @temporary-state))
+
+(defn ro-deep-merge-with-temp
+  "Deep-merges the temporary state with the global read-only one.
+
+  Refer to bzzz.state.ro-deep-merge for details"
+  []
+  (ro-deep-merge @temporary-state))
--- test/bzzz/state_test.clj
+++ test/bzzz/state_test.clj
@@ -6,9 +6,13 @@
 (defn ro-get-in [k & [default]]
   (get-in TermPayloadClojureScoreQuery/EXPR_GLOBAL_STATE_RO k default))
 
-(defn ro-get-in-equals-to [k target]
+(defn ro-get-in-equals-to [k target & [msg]]
   (is (= (ro-get-in k) target)
-      (str "Global State: " TermPayloadClojureScoreQuery/EXPR_GLOBAL_STATE_RO)))
+      (str (if msg (str msg "\n") "") "Global state: " TermPayloadClojureScoreQuery/EXPR_GLOBAL_STATE_RO)))
+
+(defn temp-get-in-equals-to [k target & [msg] ]
+  (is (= (get-in @state/temporary-state k) target)
+      (str (if msg (str msg "\n") "") "Temporary state: " @state/temporary-state)))
 
 (deftest test-state
 
@@ -44,4 +48,36 @@
 
       (testing "and lets me be lousy rename using a string instead of a keyword. sigh."
         (state/ro-rename-key "after-key" "before-key")
-        (ro-get-in-equals-to [:before-key] test-data)))))
+        (ro-get-in-equals-to [:before-key] test-data))))
+
+  (testing "temporary state"
+
+    (testing "assoc-in adds keys properly"
+      (state/temp-assoc-in [ [[:foo :bar] 42] [[:foo :baz] 24] ])
+      (temp-get-in-equals-to [:foo :bar] 42)
+      (temp-get-in-equals-to [:foo :baz] 24))
+
+    (testing "replace overwrites the global state"
+      ;; erase the global state
+      (TermPayloadClojureScoreQuery/replace_expr_global_state_ro (hash-map))
+      (state/ro-replace-with-temp)
+      (ro-get-in-equals-to [:foo :bar] 42)
+      (ro-get-in-equals-to [:foo :baz] 24))
+
+    (testing "merge does the right thing"
+      ;; preload the global state
+      (TermPayloadClojureScoreQuery/replace_expr_global_state_ro { :foo { :bar 12 :foobar 1024 } })
+      (state/ro-merge-with-temp)
+      (ro-get-in-equals-to [:foo :foobar] nil "Subkeys should disapear")
+      (ro-get-in-equals-to [:foo :bar] 42 "But collisions should be replaced"))
+
+    (testing "deep-merge also does the right thing"
+      ;; preload the global state
+      (TermPayloadClojureScoreQuery/replace_expr_global_state_ro { :foo { :bar 12 :foobar 1024 } })
+      (state/ro-deep-merge-with-temp)
+      (ro-get-in-equals-to [:foo :foobar] 1024 "Old keys should not disappear")
+      (ro-get-in-equals-to [:foo :bar] 42 "But collisions should be replaced"))
+
+    (testing "and empty actually erases it"
+      (state/temp-empty)
+      (is (= @state/temporary-state {})))))
